c1c218010f1888d1a36fee823205f7b95470494a
Indra2201020002 <bioohazard2@gmail.com>
2024-01-03 19:18:40 +0800

    edit database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
